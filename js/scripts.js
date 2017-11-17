92e300e70ef9ce1c91e69c94e1ff2577db2f36fd
Katja Gebuhr <katjagebuhr@Katja.home>
2017-11-17 21:57:56 +0000

    Removed the click JS code and replaced it with Hover and Click code for the project pictures on the index.html and work.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
-  $(".project-preview").on("click", function() {
-    $(this).siblings(".project-details").slideToggle();
-  });
-});
+$(document).ready(function () {
+  $('.project').on('mouseenter', function () {
+    $(this).find('.project-details').fadeIn()
+  })
+  $('.project').on('mouseleave', function () {
+    $(this).find('.project-details').fadeOut()
+  })
+})
+
+$(document).ready(function () {
+  $('#safespace').on('click', function () {
+    window.location.href = 'safespace.html'
+  })
+})
+
+$(document).ready(function () {
+  $('#bus').on('click', function () {
+    window.location.href = 'busservice.html'
+  })
+})
